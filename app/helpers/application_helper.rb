669d0dfedc815686d241b6eac0cfd81d5ffba364
shawnbenn <info@listebyshawn.com>
2016-06-29 00:35:39 -0400

    Clean up Markdown Code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
+
 module ApplicationHelper
+	def markdown(text)
+		renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true, gh_blockcode: true)
+		options = {
+			autolink: true,
+			no_intra_emphasis: true,
+			disable_indented_code_blocks: true,
+			fenced_code_blocks: true,
+			lax_html_blocks: true,
+			strikethrough: true,
+			superscript: true,
+		}
+		syntax_highlighter(Redcarpet::Markdown.new(renderer, options).render(text)).html_safe
+	end
+
+	def syntax_highlighter(html)
+		doc = Nokogiri::HTML(html)
+		doc.search("//pre[@lang]").each do |pre|
+			pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])  
+		end
+		doc.to_s
+	end
 end
+
+# module ApplicationHelper
+# 	def markdown(text)
+# 		options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code :gh_blockcode]
+# 		Redcarpet.new(text, options).to_html.html_safe
+# 	end
+# end
