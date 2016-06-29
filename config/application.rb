d8f8ed3c3ede835c21a30b03364390a60b136294
shawnbenn <info@listebyshawn.com>
2016-06-29 16:56:01 -0400

    Switch to Heroku

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module App
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +23,8 @@ module App
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   end
 end
