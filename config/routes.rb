ffa4777ab2473afefde87b5e7b23cfc81920f1cd
shawnbenn <info@listebyshawn.com>
2016-07-01 15:23:20 -0400

    Added Sendgrid and Devise setup complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+	  devise_for :users
+	  devise_scope :user do  
+	   get '/users/sign_out' => 'devise/sessions#destroy'     
+	  end
 resources :posts
 resources :projects
 resources :contacts, only: [:new, :create]
 get 'welcome/index'
 root 'welcome#index'
+
+get '*path' => redirect('/')
 end
